4a0da823439fcddff26646f30c049e0969e1a08d
victoredu00 <victor.eduardo.sts@gmail.com>
2024-11-08 01:00:24 +0000

    Código 1.0

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,21 +1,65 @@
 #include <stdio.h>
 
-// Desafio Super Trunfo - Países
-// Tema 1 - Cadastro das Cartas
-// Este código inicial serve como base para o desenvolvimento do sistema de cadastro de cartas de cidades.
-// Siga os comentários para implementar cada parte do desafio.
+//DEFININDO A ESTRUTURA PARA ARMAZENAR INFORMAÇÕES DA CIDADE
+  typedef struct { 
+     char estado[30];
+     int codigo;
+     char nome[50];
+     int populacao;
+     double pib;
+     double area;
+     int pontos_turisticos;
+  } carta;
 
-int main() {
-    // Sugestão: Defina variáveis separadas para cada atributo da cidade.
-    // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
-    
-    // Cadastro das Cartas:
-    // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
-    // Solicite ao usuário que insira as informações de cada cidade, como o código, nome, população, área, etc.
-    
-    // Exibição dos Dados das Cartas:
-    // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
-    // Exiba os valores inseridos para cada atributo da cidade, um por linha.
+ // FUNÇÃO PARA CALCULAR A DENSIDADE POPULACIONAL
+  double calcularDensidadePopulacional(carta c) {
+    return c.populacao / c.area;
+  }
 
-    return 0;
-}
+  //FUNÇÃO PARA CALCULAR O PIB PER CAPITA 
+  double calcularPIBPerCapta(carta c) {
+    return c.populacao / c.area;
+  }
+
+  //FUNÇÃO PARA IMPRIMIR OS DADOS DA CARTA
+  void imprimirCarta(carta c) {
+    printf("Estado: %s\n", c.estado);
+    printf("Código: %d\n", c.codigo);
+    printf("Nome: %s\n", c.nome);
+    printf("População: %d\n");
+    printf("PIB: %.2f\n", c.pib);
+    printf("Área: %.2f km²\n", c.area);
+    printf("Pontos Turísticos: %d\n", c.pontos_turisticos);
+    printf("Densidade Populacional: %.2f hab/km²\n");
+    printf("PIB Per Capita: 2.2f\n", calcularPIBPerCapta(c));
+  }
+
+  int main() {
+carta cidade;
+
+//COLETA DE DADOS DO USUÁRIO VIA TERMINAL
+   printf("insira o estado: ");
+   scanf("%s", cidade.estado);
+
+   printf("Insira o código: ");
+   scanf("%d", &cidade.codigo);
+
+   printf("Insira o nome da cidade: ");
+   scanf("%s", cidade.nome),
+
+   printf("Insira a população: " ); 
+   scanf("%d", &cidade.populacao);
+
+   printf("Insira o PIB: ");
+   scanf("%lf", &cidade.pib);
+
+   printf("Insira a área (km²): ");
+   scanf("%lf", &cidade.area);
+   
+   printf("Insira o número de pontos turísticos: ");
+   scanf("%d", &cidade.pontos_turisticos);
+
+   imprimirCarta(cidade);
+        return 0;
+
+  }
